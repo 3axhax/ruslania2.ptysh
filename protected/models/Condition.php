ffb005d0beedd951323c747d0ec5382cc41dfe13
kirill <kirill.ruh@gmail.com>
2018-08-04 00:30:21 +0400

    фильтр по цене

--- protected/models/Condition.php
+++ protected/models/Condition.php
@@ -8,6 +8,16 @@ class Condition {
 	private $_onlySupportLanguage = false;
 	private $_languageCondition = array();
 
+	private $_isDiscount = array(
+		'category'=>array(),
+		'series'=>array(),
+		'publisher'=>array(),
+		'years'=>array(),
+		'entity'=>0,
+		'user'=>0,
+		'ruslania'=>0,
+	);
+
 	static private $_self = array();
 
 	final private function __construct($entity, $cid) {
@@ -96,6 +106,9 @@ class Condition {
 	}
 
 	private function _price() {
+		//TODO:: цена для PERIODIC как то хитро сделална. Буду делать позжее.
+		if ($this->_entity == Entity::PERIODIC) return;
+
 		$bMin = abs((float) $this->g('cost_min'));
 		$bMax = abs((float) $this->g('cost_max'));
 		if (($bMax > 0)&&($bMax < $bMin)) {
@@ -105,10 +118,60 @@ class Condition {
 		}
 		//TODO:: добавить конвертацию валюты
 		if (!empty($bMin)||!empty($bMax)) {
-			if (empty($bMin)) $this->_condition['brutto'] = '(t.brutto <= ' . $bMax . ')';
-			elseif (empty($bMax)) $this->_condition['brutto'] = '(t.brutto >= ' . $bMin . ')';
-			elseif ($bMin == $bMax) $this->_condition['brutto'] = '(t.brutto = ' . $bMin . ')';
-			else $this->_condition['brutto'] = '(t.brutto between ' . $bMin . ' and ' . $bMax . ')';
+			$rates = Currency::GetRates();
+			$rate = $rates[Yii::app()->currency];
+
+			$this->_fillDiscounts();
+			$allPercent = $this->_isDiscount['user'];
+			if (!empty($this->_isDiscount['entity'])) $allPercent = max($this->_isDiscount['user'], $this->_isDiscount['entity'], $this->_isDiscount['ruslania']);
+			$categoryPercent = $this->_getSqlBruttoCategory();
+			$seriesPercent = $this->_getSqlBrutto('series', 'series_id');
+			$publisherPercent = $this->_getSqlBrutto('publisher', 'publisher_id');
+			$yearPercent = $this->_getSqlBrutto('year', 'year');
+
+			$percents = array();
+			if (!empty($allPercent)) $percents[] = $allPercent;
+			if (!empty($categoryPercent)) $percents[] = $categoryPercent;
+			if (!empty($seriesPercent)) $percents[] = $seriesPercent;
+			if (!empty($publisherPercent)) $percents[] = $publisherPercent;
+			if (!empty($yearPercent)) $percents[] = $yearPercent;
+
+			$sqlPercent = '';
+			//что то ломает придумывать алгоритм
+			switch (count($percents)) {
+				case 1: $sqlPercent = $percents[0]; break;
+				case 2: $sqlPercent = 'if(' . $percents[0] . ' > ' . $percents[1] . ', ' . $percents[0] . ', ' . $percents[1] . ')'; break;
+				case 3: $sqlPercent = 'if(' . $percents[0] . ' > ' . $percents[1] . ', if(' . $percents[0] . ' > ' . $percents[2] . ', ' . $percents[0] . ', ' . $percents[2] . '), if(' . $percents[1]  . ' > ' . $percents[2] . ', ' . $percents[1] . ', ' . $percents[2] . '))'; break;
+				case 4: $sqlPercent = ''.
+					'case '.
+						'when (' . $percents[0] . ' >= ' . $percents[1] . ') and (' . $percents[0] . ' >= ' . $percents[2] . ') and (' . $percents[0] . ' >= ' . $percents[3] . ') then ' . $percents[0] . ' '.
+						'when (' . $percents[1] . ' >= ' . $percents[0] . ') and (' . $percents[1] . ' >= ' . $percents[2] . ') and (' . $percents[1] . ' >= ' . $percents[3] . ') then ' . $percents[1] . ' '.
+						'when (' . $percents[2] . ' >= ' . $percents[1] . ') and (' . $percents[2] . ' >= ' . $percents[0] . ') and (' . $percents[2] . ' >= ' . $percents[3] . ') then ' . $percents[2] . ' '.
+					'else ' . $percents[3] . ' end '.
+				'';
+					break;
+				case 5: $sqlPercent = ''.
+					'case '.
+						'when (' . $percents[0] . ' >= ' . $percents[1] . ') and (' . $percents[0] . ' >= ' . $percents[2] . ') and (' . $percents[0] . ' >= ' . $percents[3] . ') and (' . $percents[0] . ' >= ' . $percents[4] . ') then ' . $percents[0] . ' '.
+						'when (' . $percents[1] . ' >= ' . $percents[0] . ') and (' . $percents[1] . ' >= ' . $percents[2] . ') and (' . $percents[1] . ' >= ' . $percents[3] . ') and (' . $percents[1] . ' >= ' . $percents[4] . ') then ' . $percents[1] . ' '.
+						'when (' . $percents[2] . ' >= ' . $percents[1] . ') and (' . $percents[2] . ' >= ' . $percents[0] . ') and (' . $percents[2] . ' >= ' . $percents[3] . ') and (' . $percents[2] . ' >= ' . $percents[4] . ') then ' . $percents[2] . ' '.
+						'when (' . $percents[3] . ' >= ' . $percents[1] . ') and (' . $percents[2] . ' >= ' . $percents[0] . ') and (' . $percents[2] . ' >= ' . $percents[3] . ') and (' . $percents[3] . ' >= ' . $percents[4] . ') then ' . $percents[2] . ' '.
+					'else ' . $percents[4] . ' end '.
+				'';
+					break;
+			}
+
+			if (empty($sqlPercent)) $brutto = 'if((ifnull(t.discount, 0) > 0) and (t.brutto > ifnull(t.discount, 0)), t.discount, t.brutto)';
+			else {
+				$brutto = '(t.brutto - (' . $sqlPercent . ')*t.brutto/100)';
+				$brutto = 'if((ifnull(t.discount, 0) > 0) and (' . $brutto . ' > t.discount), t.discount, ' . $brutto . ')';
+			}
+
+
+			if (empty($bMin)) $this->_condition['brutto'] = '(' . $brutto . ' <= ' . $bMax / $rate . ')';
+			elseif (empty($bMax)) $this->_condition['brutto'] = '(' . $brutto . ' >= ' . $bMin / $rate . ')';
+			elseif ($bMin == $bMax) $this->_condition['brutto'] = '(' . $brutto . ' = ' . $bMin / $rate . ')';
+			else $this->_condition['brutto'] = '(' . $brutto . ' between ' . $bMin / $rate . ' and ' . $bMax / $rate . ')';
 		}
 	}
 
@@ -187,7 +250,9 @@ class Condition {
 				if (empty($this->_join)&&(($countCond = count($this->_condition)) <= 3)) {
 					if (!empty($this->_condition['cid'])) $countCond--;
 					if (!empty($this->_condition['year'])) $countCond--;
-					if (!empty($this->_condition['brutto'])) $countCond--;
+					if (!empty($this->_condition['brutto'])) {
+						if (mb_strpos($this->_condition['brutto'], 't.code', null, 'utf-8') === false) $countCond--;
+					}
 					if ($countCond === 0) $this->_onlySupportLanguage = true;
 				}
 				$this->_languageCondition = array('language_id'=>'(t.language_id = ' . $langsel . ')');
@@ -219,6 +284,136 @@ class Condition {
 
 			}
 		}
+	}
+
+	private function _fillDiscounts() {
+		$this->_isDiscount['user'] = Yii::app()->user->id?Yii::app()->user->GetPersonalDiscount():0;
+		$maxDiscount = $this->_isDiscount['user'];
+		$dateStart = $dateEnd = date('Y-m-d');
+//		$dateEnd = '2017-06-01';
+
+		$sql = 'select type_id, max(discount) discount '.
+			'from discounts '.
+			'where (ifnull(entity_id, 0) in (0, ' . $this->_entity . ')) '.
+				'and (ifnull(discount, 0) > ' . $maxDiscount . ') '.
+				'and (start_date <= "' . $dateStart . '") '.
+				'and (end_date >= "' . $dateEnd . '") '.
+			'group by type_id '.
+		'';
+		$discounts = Yii::app()->db->createCommand($sql)->queryAll();
+
+		//надо 2 раза пробежаться по массиву скидок, чтобы сначала получить максимальную скидку на все товары
+		$maxDiscount = $this->_fillDiscountAll($discounts, $maxDiscount);
+		//а второй раз скидку на категорию (серию, тд) больше чем общая скидка
+		foreach ($discounts as $discount) {
+			$idTypeDiscount = (int) $discount['type_id'];
+			switch ($idTypeDiscount) {
+				case 2: $this->_fillDiscountCategory($idTypeDiscount, $dateStart, $dateEnd, $maxDiscount); break;
+				case 3: $this->_fillDiscountParams('series', 'serie_id', $idTypeDiscount, $dateStart, $dateEnd, $maxDiscount); break;
+				case 4: $this->_fillDiscountParams('publisher', 'publisher_id', $idTypeDiscount, $dateStart, $dateEnd, $maxDiscount); break;
+				case 9: $this->_fillDiscountParams('years', 'year', $idTypeDiscount, $dateStart, $dateEnd, $maxDiscount); break;
+			}
+		}
+	}
+
+	private function _fillDiscountAll($discounts, $maxDiscount) {
+		foreach ($discounts as $discount) {
+			$idTypeDiscount = (int) $discount['type_id'];
+			switch ($idTypeDiscount) {
+				case 1:
+					$this->_isDiscount['ruslania'] = (float) $discount['discount'];
+					if ($this->_isDiscount['ruslania'] > $maxDiscount) {
+						$maxDiscount = $this->_isDiscount['ruslania'];
+						$this->_isDiscount['user'] = 0;
+						$this->_isDiscount['entity'] = 0;
+					}
+					break;
+				case 10:
+					$this->_isDiscount['entity'] = (float) $discount['discount'];
+					if ($this->_isDiscount['entity'] > $maxDiscount) {
+						$maxDiscount = $this->_isDiscount['entity'];
+						$this->_isDiscount['user'] = 0;
+						$this->_isDiscount['ruslania'] = 0;
+					}
+					break;
+			}
+		}
+		return $maxDiscount;
+	}
+
+	private function _fillDiscountParams($paramName, $paramId, $idTypeDiscount, $dateStart, $dateEnd, $maxDiscount) {
+		if (!Entity::checkEntityParam($this->_entity, $paramName)) return;
+		$sql = 'select ' . $paramId . ', discount '.
+			'from discounts '.
+			'where (entity_id = ' . $this->_entity . ') '.
+				'and (type_id = ' . $idTypeDiscount . ') '.
+				'and (start_date <= "' . $dateStart . '") '.
+				'and (end_date >= "' . $dateEnd . '") '.
+				'and (ifnull(discount, 0) > ' . $maxDiscount . ') '.
+		'';
+		foreach (Yii::app()->db->createCommand($sql)->queryAll() as $row) {
+			if (empty($this->_isDiscount[$paramName][$row[$paramId]])) $this->_isDiscount[$paramName][$row[$paramId]] = 0;
+			$this->_isDiscount[$paramName][$row[$paramId]] = max($this->_isDiscount[$paramName][$row[$paramId]], (float) $row['discount']);
+		}
+	}
+
+	private function _fillDiscountCategory($idTypeDiscount, $dateStart, $dateEnd, $maxDiscount) {
+		$sql = 'select category_id, discount '.
+			'from discounts '.
+			'where (entity_id = ' . $this->_entity . ') '.
+				'and (type_id = ' . DiscountManager::TYPE_PART . ') '.
+				'and (start_date <= "' . $dateStart . '") '.
+				'and (end_date >= "' . $dateEnd . '") '.
+				'and (ifnull(discount, 0) > ' . $maxDiscount . ') '.
+		'';
+		foreach (Yii::app()->db->createCommand($sql)->queryAll() as $row) {
+			$row['discount'] = (float) $row['discount'];
+			$c = new Category;
+			$childIds = $c->GetChildren($this->_entity, $row['category_id']);
+			array_push($childIds, $row['category_id']);
+			if (empty($this->_cid)||in_array($this->_cid, $childIds)) {
+				foreach ($childIds as $cid) {
+					if (empty($this->_isDiscount['category'][$cid])) $this->_isDiscount['category'][$cid] = 0;
+					$this->_isDiscount['category'][$cid] = max($this->_isDiscount['category'][$cid], $row['discount']);
+				}
+			}
+		}
+	}
+
+	private function _getSqlBrutto($paramName, $paramId) {
+		if (!Entity::checkEntityParam($this->_entity, $paramName)) return '';
+
+		$percents = array();
+		$result = array();
+		foreach ($this->_isDiscount[$paramName] as $id=>$percent) {
+			if (empty($percents[$percent])) $percents[$percent] = array();
+			$percents[$percent][] = $id;
+		}
+		foreach ($percents as $percent=>$ids) {
+			$result[] = 'if(t.' . $paramId . ' in (' . implode(',', $ids) . '), "' . $percent . '", ';
+		}
+		if (!empty($result)) {
+			$result = implode($result) . '0' . str_repeat(')', count($result));
+		}
+		else $result = '';
+		return $result;
+	}
+
+	private function _getSqlBruttoCategory() {
+		$percents = array();
+		$result = array();
+		foreach ($this->_isDiscount['category'] as $id=>$percent) {
+			if (empty($percents[$percent])) $percents[$percent] = array();
+			$percents[$percent][] = $id;
+		}
+		foreach ($percents as $percent=>$ids) {
+			$result[] = 'if((t.code in (' . implode(',', $ids) . ')) or (t.subcode in (' . implode(',', $ids) . ')), "' . $percent . '", ';
+		}
+		if (!empty($result)) {
+			$result = implode($result) . '0' . str_repeat(')', count($result));
+		}
+		else $result = '';
+		return $result;
 	}
 
 }
