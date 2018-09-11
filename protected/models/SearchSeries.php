74a2e500d6c6f4f7ffd56c7f98a43ea0b6d51200
3axhax <3axhax@gmail.com>
2018-09-11 08:54:25 +0500

    Язык в "Подписке", Селект с подсказкой для выбора серии.

--- protected/models/SearchSeries.php
+++ protected/models/SearchSeries.php
@@ -45,28 +45,69 @@ class SearchSeries
         foreach ($rows as $row) {
             if (mb_stripos($row['title_ru'], $q) !== false) {
                 $series[$i]['id'] = $row['series_id'];
-                $series[$i]['title'] = $row['title_ru'];
+                $series[$i]['title'] = htmlspecialchars($row['title_ru']);
                 $i++;
                 continue;
             }
             if (mb_stripos($row['title_rut'], $q) !== false) {
                 $series[$i]['id'] = $row['series_id'];
-                $series[$i]['title'] = $row['title_rut'];
+                $series[$i]['title'] = htmlspecialchars($row['title_rut']);
                 $i++;
                 continue;
             }
             if (mb_stripos($row['title_en'], $q) !== false) {
                 $series[$i]['id'] = $row['series_id'];
-                $series[$i]['title'] = $row['title_en'];
+                $series[$i]['title'] = htmlspecialchars($row['title_en']);
                 $i++;
                 continue;
             }
             if (mb_stripos($row['title_fi'], $q) !== false) {
                 $series[$i]['id'] = $row['series_id'];
-                $series[$i]['title'] = $row['title_fi'];
+                $series[$i]['title'] = htmlspecialchars($row['title_fi']);
                 $i++;
                 continue;
             }
+
+        }
+        return $series;
+    }
+
+    function  getSeriesSelectFilters($entity, $cid = 0) {
+        if (!Entity::checkEntityParam($entity, 'series')) return array();
+
+        $entities = Entity::GetEntitiesList();
+        $tbl = $entities[$entity]['site_table'];
+        $series_tbl = $entities[$entity]['site_series_table'];
+
+        $filter_data = FilterHelper::getFiltersData($entity, $cid);
+
+        if ($cid > 0) {
+            $sql = 'SELECT tc.series_id, st.title_ru, st.title_rut, st.title_en, st.title_fi 
+            FROM '.$series_tbl.' as st 
+            LEFT JOIN ' . $tbl . ' as tc   
+            ON (tc.series_id=st.id)
+            WHERE tc.avail_for_order='.$filter_data['avail'].' AND (tc.`code`=:code OR tc.`subcode`=:code) 
+            GROUP BY tc.series_id';
+            $rows = Yii::app()->db->createCommand($sql)->queryAll(true, array(':code' => $cid));
+        } else {
+            $sql = 'SELECT tc.series_id, st.title_ru, st.title_rut, st.title_en, st.title_fi 
+            FROM '.$series_tbl.' as st 
+            LEFT JOIN ' . $tbl . ' as tc   
+            ON (tc.series_id=st.id)
+            WHERE tc.avail_for_order='.$filter_data['avail'].'
+            GROUP BY tc.series_id';
+            $rows = Yii::app()->db->createCommand($sql)->queryAll(true);
+        }
+        $series = [];
+        $i = 0;
+        foreach ($rows as $row) {
+            $series[(int)$row['series_id']]['ru'] = $row['title_ru'];
+            if ($row['title_rut'] != $row['title_ru'])
+                $series[(int)$row['series_id']]['rut'] = $row['title_rut'];
+            if ($row['title_en'] != $row['title_ru'] && $row['title_en'] != $row['title_rut'])
+                $series[(int)$row['series_id']]['en'] = $row['title_en'];
+            if ($row['title_fi'] != $row['title_ru'] && $row['title_fi'] != $row['title_rut'] && $row['title_fi'] != $row['title_en'])
+                $series[(int)$row['series_id']]['fi'] = $row['title_fi'];
 
         }
         return $series;
