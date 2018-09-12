b075056d2966107fd7ae8c0fad486d663dec2452
3axhax <3axhax@gmail.com>
2018-09-12 09:30:35 +0500

    Заполнение фильтира в "Видео".

--- protected/components/FilterHelper.php
+++ protected/components/FilterHelper.php
@@ -395,6 +395,13 @@ class FilterHelper
     }
 
     static private function getFormatVideo() {
+        $format_video = false;
+        if (Yii::app()->getController()->action->id == 'byaudiostream')
+            $format_video = Yii::app()->getRequest()->getParam('sid', false);
+        if ($format_video !== false) {
+            self::$data['format_video'] = (int) $format_video;
+            return true;
+        }
         $format_video = Yii::app()->getRequest()->getParam('format_video', false);
         if ($format_video !== false) {
             self::$data['format_video'] = (int) $format_video;
@@ -409,6 +416,13 @@ class FilterHelper
     }
 
     static private function getLangVideo() {
+        $lang_video = false;
+        if (Yii::app()->getController()->action->id == 'byaudiostream')
+            $lang_video = Yii::app()->getRequest()->getParam('sid', false);
+        if ($lang_video !== false) {
+            self::$data['lang_video'] = (int) $lang_video;
+            return true;
+        }
         $lang_video = Yii::app()->getRequest()->getParam('lang_video', false);
         if ($lang_video !== false) {
             self::$data['lang_video'] = (int) $lang_video;
@@ -423,6 +437,13 @@ class FilterHelper
     }
 
     static private function getSubtitlesVideo() {
+        $subtitles_video = false;
+        if (Yii::app()->getController()->action->id == 'bysubtitle')
+            $subtitles_video = Yii::app()->getRequest()->getParam('sid', false);
+        if ($subtitles_video !== false) {
+            self::$data['subtitles_video'] = (int) $subtitles_video;
+            return true;
+        }
         $subtitles_video = Yii::app()->getRequest()->getParam('subtitles_video', false);
         if ($subtitles_video !== false) {
             self::$data['subtitles_video'] = (int) $subtitles_video;
