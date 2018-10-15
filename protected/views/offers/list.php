2764844822930e5a6dd8b55c7b818a1fbdd7b4a8
3axhax <3axhax@gmail.com>
2018-10-15 21:28:47 +0500

    В рекомендациях все разделы

--- protected/views/offers/list.php
+++ protected/views/offers/list.php
@@ -32,21 +32,7 @@
 							//var_dump(Entity::GetTitle($entity));
 							
 							//echo Entity::GetTitle($entity);
-							
-							
-							$title_item = '';
-							switch ($entity) {
-								case 10: $title_item = $ui->item('A_NEW_POP1'); break;
-								case 15: $title_item = $ui->item('A_NEW_POP2'); break;
-								case 20: $title_item = $ui->item('A_NEW_POP3'); break;
-								case 22: $title_item = $ui->item('A_NEW_POP4'); break;
-								case 24: $title_item = $ui->item('A_NEW_POP5'); break;
-								case 30: $title_item = $ui->item('A_NEW_POP6'); break;
-								case 40: $title_item = $ui->item('A_NEW_POP7'); break;
-								case 50: $title_item = $ui->item('A_NEW_POP8'); break;
-								case 60: $title_item = $ui->item('A_NEW_POP9'); break;
-								default: $title_item = $ui->item('A_NEW_POP10'); break;
-							}
+
 							
 							$s = 0;
 							//var_dump($item);
@@ -55,7 +41,7 @@
 					
 					
                         <div>
-<a class="title_item_recomend" href="<?=Yii::app()->createUrl('offers/view', array('oid' => $item['id'], 'title' => ProductHelper::ToAscii($title))); ?>"><?= /*$title_item*/CHtml::encode($title) ?>!</a>
+<a class="title_item_recomend" href="<?=Yii::app()->createUrl('offers/view', array('oid' => $item['id'], 'title' => ProductHelper::ToAscii($title))); ?>"><?= CHtml::encode($title) ?></a>
 <span class="date_recomend"><?=Yii::app()->dateFormatter->format('dd MMM yyyy', $item['creation_date']); ?></span>
                         </div>
 <?=CHtml::encode(ProductHelper::GetDescription($item->attributes)); ?>
@@ -64,19 +50,21 @@
 							if (count($offer[Entity::GetTitle($entity)]['items'])) {
 								echo '<div class="items_goods_recomends">';
 								echo '<div class="slider_recomend custom-slider">';
-							foreach ($offer[Entity::GetTitle($entity)]['items'] as $of) {
+								foreach ($offer as $offer_entity) {
+                                    foreach ($offer_entity['items'] as $of) {
 
-                                //if ($s < 7) {
-                                if (true) {
-                                    echo '<div class="item slider_recomend__item">';
-                                    echo '<a href="' . ProductHelper::createUrl($of) . '" class="slider__img-block">
+                                        //if ($s < 7) {
+                                        if (true) {
+                                            echo '<div class="item slider_recomend__item">';
+                                            echo '<a href="' . ProductHelper::createUrl($of) . '" class="slider__img-block">
 										<div class="img slider__img" style="background: url(\'' . Picture::Get($of, Picture::SMALL) . '\') center center no-repeat; background-size: 100%; position: relative">';
-                                    $this->renderStatusLables(Product::GetStatusProduct($of['entity'], $of['id']), '', true);
-                                    echo '</div></a>';
-                                    echo '</div>';
+                                            $this->renderStatusLables(Product::GetStatusProduct($of['entity'], $of['id']), '', true);
+                                            echo '</div></a>';
+                                            echo '</div>';
+                                        }
+                                        $s++;
+                                    }
                                 }
-                                $s++;
-                            }
 								echo '</div><div class="clearfix"></div></div>';
 							}
 							?><div style="margin-top: 15px;"></div>
