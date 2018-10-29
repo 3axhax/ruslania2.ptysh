faf6296a93dcf1e51718753423e1de49203c9fbb
kirill <kirill.ruh@gmail.com>
2018-10-29 23:01:08 +0400

    баннер на главной

--- protected/components/widgets/views/banners_main_small.php
+++ protected/components/widgets/views/banners_main_small.php
@@ -6,14 +6,15 @@
 	$productPicture = Picture::Get($offerDay, Picture::SMALL);
 	$productTitle = ProductHelper::GetTitle($offerDay, 'title');
 	?>
-		<div class="span6 main-banner-content" style="background: url(/new_img/poslozhka.gif) 100% 100% no-repeat; background-size: contain;">
+		<div class="span6 main-banner-content" style="background: url(/new_img/poslozhka.jpg) 100% 100% no-repeat; background-size: contain;">
 			<div class="photo">
 				<a href="<?= $url ?>"><img src="<?= $productPicture ?>" alt=""/></a>
 			</div>
 	<?php if (!empty($offerDay['priceData'][DiscountManager::DISCOUNT])) : ?>
 			<div class="discount"><?= Yii::app()->ui->item('PRODUCT_OF_DAY_INFO', $offerDay['priceData'][DiscountManager::DISCOUNT]) ?></div>
 	<?php endif; ?>
-			<div class="title"><a href="<?= $url ?>"><?= $productTitle ?><?php /*<span class="gradient_link"></span> */ ?></a></div>
+			<div class="title"><div><a href="<?= $url ?>"><?= $productTitle ?><?php /*<span class="gradient_link"></span> */ ?></a></div></div>
+<?php /*
 			<div class="cost">
                 <?= Yii::app()->ui->item('CART_COL_PRICE') ?>
                 <span><?= ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::WITH_VAT]); ?>
@@ -23,6 +24,11 @@
 				(<?= ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::WITHOUT_VAT]); ?>
 				<?= $offerDay['priceData']['unit'] ?>
 				<?=Yii::app()->ui->item('WITHOUT_VAT'); ?>)
+			</div>
+*/ ?>
+			<div class="cost_nds">
+<?= Yii::app()->ui->item('CART_COL_PRICE') ?> <?= ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::WITH_VAT]); ?> <?= $offerDay['priceData']['unit'] ?>
+(<?= ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::WITHOUT_VAT]); ?> <?= $offerDay['priceData']['unit'] ?> <?=Yii::app()->ui->item('WITHOUT_VAT'); ?>)
 			</div>
 		</div>
 <?php elseif (!empty($leftBanner)): ?>
