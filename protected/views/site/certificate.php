01e2eb493655e429605be3e8bfc1a19d644819ec
kirill <kirill.ruh@gmail.com>
2018-11-02 22:17:47 +0400

    подарочный сертификат

--- protected/views/site/certificate.php
+++ protected/views/site/certificate.php
@@ -8,6 +8,9 @@ $h1 = mb_strtoupper(mb_substr($h1, 0, 1, 'utf-8')) . mb_substr($h1, 1, null, 'ut
 
 KnockoutForm::RegisterScripts();
 
+$nominals = array();
+for ($i=1;$i<=100;$i++) $nominals[$i] = $i;
+
 /*
  * Вид сертификата
  * Имя
@@ -18,6 +21,7 @@ KnockoutForm::RegisterScripts();
  * Текст Вашего сообщения (будет отпечатан на сертификате)
  * Стоимость
  * */
+//Currency::ToSign(Yii::app()->currency)
 ?>
 
 <div class="container view_product">
@@ -30,37 +34,38 @@ KnockoutForm::RegisterScripts();
 					<?= CHtml::beginForm(Yii::app()->createUrl('site/certificate'), 'post'); ?>
 					<div class="form_row">
 						<div class="row_name">Вид сертификата</div>
-						<div class="row_value"><?= CHtml::dropDownList('gift[maket]', 1, array(1=>1,2=>2,3=>3), array('style'=>'width: 240px;')); ?></div>
+						<div class="row_value"><?= CHtml::dropDownList('gift[maket]', 1, array(1=>1,2=>2,3=>3), array('style'=>'width: 50px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Имя<span>*</span></div>
-						<div class="row_value"><?= CHtml::textField('gift[fio]', '', array('style'=>'width: 240px;')); ?></div>
+						<div class="row_name"><span>*</span>Имя получателя</div>
+						<div class="row_value"><?= CHtml::textField('gift[fio_dest]', '', array('style'=>'width: 240px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Адрес<span>*</span></div>
-						<div class="row_value"><?= CHtml::textField('gift[address]', '', array('style'=>'width: 240px;')); ?></div>
+						<div class="row_name"><span>*</span>E-mail получателя</div>
+						<div class="row_value"><?= CHtml::textField('gift[email_dest]', '', array('style'=>'width: 240px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Город<span>*</span></div>
-						<div class="row_value"><?= CHtml::textField('gift[city]', '', array('style'=>'width: 240px;')); ?></div>
+						<div class="row_name">Текст сообщения</div>
+						<div class="row_value"><?= CHtml::textArea('gift[txt_dest]', '', array('style'=>'height: 100px; width: 240px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Штат<span>*</span></div>
-						<div class="row_value"><?= CHtml::textField('gift[state]', '', array('style'=>'width: 240px;')); ?></div>
+						<div class="row_name"><span>*</span>Имя отправителя</div>
+						<div class="row_value"><?= CHtml::textField('gift[fio_source]', '', array('style'=>'width: 240px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Почтовый индекс<span>*</span></div>
-						<div class="row_value"><?= CHtml::textField('gift[zipcode]', '', array('style'=>'width: 240px;')); ?></div>
+						<div class="row_name"><span>*</span>E-mail отправителя</div>
+						<div class="row_value"><?= CHtml::textField('gift[email_source]', '', array('style'=>'width: 240px;')); ?></div>
 					</div>
 					<div class="form_row">
-						<div class="row_name">Текст Вашего сообщения (будет отпечатан на сертификате)</div>
-						<div class="row_value"><?= CHtml::textArea('gift[txt]', '', array('style'=>'height: 100px; width: 240px;')); ?></div>
+						<div class="row_name"><span>*</span>Номинал</div>
+						<div class="row_value">
+							<?= CHtml::dropDownList('gift[nominal]', 50, $nominals, array('style'=>'width: 50px;')); ?>&nbsp;<?= Currency::ToSign(1/*Yii::app()->currency*/) ?>
+						</div>
 					</div>
-					<!--<div class="form_row">
-						<div class="row_name">Стоимость</div>
-						<div class="row_value"></div>
-						<div class="clearBoth"></div>
-					</div>-->
+					<div class="form_row">
+						<div class="row_name"></div>
+						<div class="row_value"><?= CHtml::submitButton('Купить', array('style'=>'width: 180px; background-color: #5bb75b; border-radius: 4px; border: 0; padding: 9px 0; text-align: center; font-size: 14px; color: #fff; font-weight: bold;')) ?></div>
+					</div>
 					<?= CHtml::endForm(); ?>
 				</div>
 			</div>
