4e2763150ba9941f343947239ff88e034be2270b
oavdfaktor <sankes@list.ru>
2018-11-12 20:15:35 +0300

    Исправил тексты

--- protected/views/cart/delivery2.php
+++ protected/views/cart/delivery2.php
@@ -12,7 +12,7 @@ foreach ($items as $item) {
 
     $text_placegolder = '';
 
-    if ($i == 1 AND ($_POST['id_country'] == 68 OR $_POST['id_country'] == 62)) {
+    if ($items[0]['value'] == 7 AND ($_POST['id_country'] == 68 OR $_POST['id_country'] == 62)) {
 
      $text_placegolder1 = $ui->item('DELIVERY_ECONOMY_FINEST');
      $text_placegolder2 = $ui->item('DELIVERY_PRIORITY_FINEST');
@@ -21,15 +21,29 @@ foreach ($items as $item) {
      $use = true;
      $oncange = ' onchange="$(\'.smartpost_index\').val(\'\'); $(\'.box_smartpost\').html(\'\'); $(\'.select_dd_box\').show(); $(\'.selt .check\').removeClass(\'active\'); $(\'.check\', $(this).parent()).addClass(\'active\');"';
      
-    } elseif ($i > 1  AND ($_POST['id_country'] == 68 OR $_POST['id_country'] == 62)) {
+    } elseif ($items[0]['value'] == 7  AND ($_POST['id_country'] == 68 OR $_POST['id_country'] == 62)) {
 
         $text_placegolder2 = $ui->item('DELIVERY_PRIORITY_FINEST');
         $text_placegolder3 = $ui->item('DELIVERY_EXPRESS_FINEST');
 
+        $oncange = ' onchange="$(\'.smartpost_index\').val(\'\'); $(\'.box_smartpost\').html(\'\'); $(\'.select_dd_box\').hide(); $(\'.selt .check\').removeClass(\'active\'); $(\'.check\', $(this).parent()).addClass(\'active\');"';
+
     } else {
-        $text_placegolder1 = $ui->item('DELIVERY_ECONOMY_OTHER');
-        $text_placegolder2 = $ui->item('DELIVERY_PRIORITY_OTHER');
-        $text_placegolder3 = $ui->item('DELIVERY_EXPRESS_OTHER');
+        if ($items[0]['value'] < 15) {
+
+            $text_placegolder1 = $ui->item('DELIVERY_ECONOMY_OTHER');
+            $text_placegolder2 = $ui->item('DELIVERY_PRIORITY_OTHER');
+            $text_placegolder3 = $ui->item('DELIVERY_EXPRESS_OTHER');
+
+        } elseif($items[0]['value'] > 14) {
+
+            $text_placegolder1 = $ui->item('DELIVERY_ECONOMY_OTHER_YES');
+            $text_placegolder2 = $ui->item('DELIVERY_PRIORITY_OTHER_YES');
+            $text_placegolder3 = $ui->item('DELIVERY_EXPRESS_OTHER_YES');
+
+        }
+
+
 
         $oncange = ' onchange="$(\'.smartpost_index\').val(\'\'); $(\'.box_smartpost\').html(\'\'); $(\'.select_dd_box\').hide(); $(\'.selt .check\').removeClass(\'active\'); $(\'.check\', $(this).parent()).addClass(\'active\');"';
     
@@ -51,7 +65,7 @@ foreach ($items as $item) {
 
  echo '</div>';
 
- echo '<label class="selt span3" rel="'.$item['value'].'" valute="'.Currency::ToSign($item['currency']).'" onclick="check_delivery($(this))">';
+ echo '<label class="selt span3 selt'.$i.'" rel="'.$item['value'].'" valute="'.Currency::ToSign($item['currency']).'" onclick="check_delivery($(this))">';
 
   echo '<div class="red_checkbox" style="float: right;">
             <span class="checkbox" style="height: 10px; padding-top: 2px;"><span class="check"></span></span> 
@@ -69,16 +83,18 @@ echo '<div class="clearfix"></div>
             
             <div class="select_dd">
                 
-                <div class="p2" style="font-size: 16px; font-weight: bold;margin-bottom: 5px; display: inline-block">Введите свой индекс</div> <div style="display: inline-block;border-radius: 50%;background-color: #edb421;padding: 5px;width: 18px;font-weight:  bold;height: 18px;font-size: 17px;text-align: center;line-height: 18px;margin-left: 15px; cursor: pointer;" onclick="$(\'.info_box_smart\').toggle(); $(\'.info_box_smart\').css(\'left\', $(this).offset().left); $(\'.info_box_smart\').css(\'top\', \'6px\'); $.get(\'/text_info_smart.html\', { id : 1 }, function (data) { $(\'.info_box_smart\').html(data) })" class="qbtn"> ? </div>
+                <div class="p2" style="font-size: 16px; font-weight: bold;margin-bottom: 5px; display: inline-block">Введите свой индекс, если хотите доставку в пункт выдачи.</div> <div style="display: inline-block;border-radius: 50%;background-color: #edb421;padding: 5px;width: 18px;font-weight:  bold;height: 18px;font-size: 17px;text-align: center;line-height: 18px;margin-left: 15px; cursor: pointer;" onclick="$(\'.info_box_smart\').toggle(); $(\'.info_box_smart\').css(\'top\', \'6px\'); $.get(\'/text_info_smart.html\', { id : 1 }, function (data) { $(\'.info_box_smart\').html(data) })" class="qbtn"> ? </div>
                 
-                <div style="background-color: #fff; position: absolute; display: none; padding: 20px; width: 800px; z-index: 99999; border-radius: 2px; box-shadow: 0 0 10px rgba(0,0,0,0.3)" class="info_box_smart"></div>
+                <div style="background-color: #fff; position: absolute; display: none; padding: 20px; width: 580px; right: 0; z-index: 99999; border-radius: 2px; box-shadow: 0 0 10px rgba(0,0,0,0.3)" class="info_box_smart"></div>
                 <div></div>
-                
-                Вы можете выбрать для вас удобный пункт выдачи. Введите ваш почтовый индекс в окно и нажмите "Найти". Вам покажем список пунктов выдачи и пунктов СмартПост. Если не найдете себе удобного место, попробуйте ввести другой почтовый индекс.
+              <br />  
+ Оставьте поле пустым, если хотите доставку на местную почту.<br /><br />
+
+Вы можете выбрать удобный для вас пункт выдачи. Введите ваш почтовый индекс в окно и нажмите "Найти". На экране появится список пунктов выдачи почты и пунктов СмартПост. Если не найдете удобного для себя места, попробуйте ввести другой почтовый индекс.<br /><br />
 
 
                 <div style="height: 10px;"></div>
-                <input class="smartpost_index" type="text" placeholder="" style="margin: 0;" onclick="$(\'input[name=dtid]\').slice(0,1).attr(\'checked\', \'true\')" onkeyup="if (event.keyCode==13) { search_smartpost() }">
+                <input class="smartpost_index" type="text" placeholder="Введите свой почтовый индекс" style="margin: 0;" onclick="$(\'input[name=dtid]\').slice(0,1).attr(\'checked\', \'true\')" onkeyup="if (event.keyCode==13) { search_smartpost() }">
  
                 <a href="javascript:;" class="btn btn-success start-search-smartpost" style="margin-left: 10px;" onclick="search_smartpost()">Найти</a>
             
