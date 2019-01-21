b19ab61f7cfc80df9ee41502192e0e0eec89ca20
oavdfaktor <sankes@list.ru>
2019-01-21 12:11:22 +0300

    Внедрение констант корзины

--- protected/views/cart/variants.php
+++ protected/views/cart/variants.php
@@ -4,7 +4,9 @@
 ?>
 
 <div class="container">
-
+	
+	<h1 style="margin-bottom: 50px; text-align: center;"><?=$ui->item('CARTNEW_TITLE_PAGE_VARIANT')?></h1>
+	
     <div class="span6" style="margin-left: 50px;">
        <!--<h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 25px;">Вход</h1>-->
        
@@ -19,7 +21,7 @@
             'beforeAjaxSubmit' => 'beforeAjax',
         ));
         ?>
-       <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 25px;">Вход</h1>
+       <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 25px;"><?=$ui->item('USER_LOGIN_ALT')?></h1>
         <?php
         $cls = 'login2';
         if (isset($class))
@@ -31,7 +33,7 @@
         </ul>
 
 <?= $form->textField('login', array('placeholder' => $ui->item('regform_email'))); ?>
-<?= $form->passwordField('pwd', array('placeholder' => $ui->item('regform_password'))); ?><a href="<?= Yii::app()->createUrl('site/forgot'); ?>" title="<?= $ui->item('A_REMIND_PASS'); ?>"><?= $ui->item('A_REMIND_PASS'); ?></a>  <a href="<?= Yii::app()->createUrl('site/register'); ?>" title="Регистрация" style="float: right"><?= $ui->item('regform_password') ?></a>
+<?= $form->passwordField('pwd', array('placeholder' => $ui->item('regform_password'))); ?><a href="<?= Yii::app()->createUrl('site/forgot'); ?>" title="<?= $ui->item('A_REMIND_PASS'); ?>"><?= $ui->item('A_REMIND_PASS'); ?></a>  <a href="<?= Yii::app()->createUrl('site/register'); ?>" title="Регистрация" style="float: right"><?= $ui->item('A_REGISTER') ?></a>
 
         <div style="margin-top: 10px;"><?= $form->submitButton($ui->item('A_SIGNIN'), array('class' => 'sort')); ?></div>
 
@@ -69,7 +71,7 @@
         <?php
         $form = $this->beginWidget('KnockoutForm', array(
             'model' => new User,
-            'action' => '/site/login',
+            'action' => Yii::app()->createUrl('site/login'),
             'class' => 'registr2',
             'id' => 'user-login',
             'viewModel' => 'loginVM',
@@ -77,8 +79,8 @@
             'beforeAjaxSubmit' => 'beforeAjax',
         ));
         ?>
-        <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 25px;">Нет учётной записи?</h1>
-        <a href="/cart/noregister/" class="order_start" style="width: 248px; margin: 0 auto; display: block">Продолжить без авторизации</a>
+        <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 25px;"><?=$ui->item('CARTNEW_LABEL_NO_ACCOUNT')?></h1>
+        <a href="<?=Yii::app()->createUrl('cart/noregister/')?>" class="order_start" style="width: 248px; margin: 0 auto; display: block"><?=$ui->item('CARTNEW_LABEL_CONTINUE_WITHOUT_AUTHORIZATION')?></a>
         
         <?php $this->endWidget(); ?>
         
@@ -86,7 +88,7 @@
 
         <div class="span6" style="margin-left: 0;">
     <div style="height: 20px;"></div>
-    <center> <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 2px; font-size: 16px;">Войти с помощью социальных сетей</h1></center>
+    <center> <h1 class="h1_reg" style="margin-top: 0px; margin-bottom: 2px; font-size: 16px;"><?=$ui->item('CARTNEW_LOGIN_SOCIAL')?></h1></center>
 
     <div style="margin:0 auto; width: 260px;" id="uLogin" data-ulogin="display=panel;theme=classic;fields=first_name,last_name,email;providers=vkontakte,odnoklassniki,googleplus,facebook,twitter,instagram;redirect_uri=<?=urlencode('/ulogin.php')?>;mobilebuttons=0;"></div>
     </div>
