2f32c18cf5fc7684ae38b828ca1af553b7e40221
kirill <kirill.ruh@gmail.com>
2019-01-21 19:01:41 +0400

    логи

--- protected/components/frameworkext/SlowSqlLogRoute.php
+++ protected/components/frameworkext/SlowSqlLogRoute.php
@@ -5,9 +5,15 @@ class SlowSqlLogRoute extends CWebLogRoute {
 	protected function render($view,$data) {
 		$startTime = 0;
 		foreach ($data as $i=>$log) {
+			$log[3] = (float)$log[3];
 			if ($i%2) {
 				$resultTime = $log[3] - $startTime;
-				if (!defined('cronAction')&&($resultTime > 1)) {
+/*				file_put_contents(Yii::getPathOfAlias('webroot') . '/test/check_sql.log', implode("\t", array(
+						date('d.m.Y H:i:s'),
+						$log[3],
+						$startTime,
+					)) . "\n", FILE_APPEND);*/
+				if (!defined('cronAction')&&($resultTime > 0.5)) {
 					file_put_contents(Yii::getPathOfAlias('webroot') . '/test/slow_sql.log', implode("\t", array(
 							date('d.m.Y H:i:s'),
 							$resultTime . 'сек',
