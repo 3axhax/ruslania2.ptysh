b98f879ed89a0d532479ea9ddbc8ee23cf4e35a3
KirillRu <kirill.ruh@gmail.com>
2019-01-26 12:00:35 +0300

    Только среди товаров имеющихся в наличии

--- protected/views/mail/forgot.php
+++ protected/views/mail/forgot.php
@@ -1,20 +1,35 @@
 <!-- Russain -->
-Уважаемый пользователь! <br>На сайт http://www.ruslania.com поступил запрос о восстановлении пароля. В соответствии с этим запросом высылаем Вам Ваш пароль по e-mail адресу, указанному Вами при регистрации.<br><br>Ваш логин: <?=$login; ?><br>Ваш пароль: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+
+<? if (Yii::app()->language == 'ru') :  ?>
+
+Уважаемый пользователь! <br>На сайт http://ruslania2.ptysh.ru поступил запрос о восстановлении пароля. В соответствии с этим запросом высылаем Вам Ваш пароль по e-mail адресу, указанному Вами при регистрации.<br><br>Ваш логин: <?=$login; ?><br>Ваш пароль: <?=$pwd; ?>
+
 <!-- Russain translit -->
-Uvazhaemyj polzovatel! <br>Na sajt http://www.ruslania.com postupil zapros o vosstanovlenii parolja. V sootvetstvii s etim zaprosom vysylaem Vam Vash parol po e-mail adresu, ukazannomu Vami pri registratsii.<br><br>Vash login: <?=$login; ?><br>Vash parol: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+<? elseif (Yii::app()->language == 'rut') : ?>
+
+
+Uvazhaemyj polzovatel! <br>Na sajt http://ruslania2.ptysh.ru postupil zapros o vosstanovlenii parolja. V sootvetstvii s etim zaprosom vysylaem Vam Vash parol po e-mail adresu, ukazannomu Vami pri registratsii.<br><br>Vash login: <?=$login; ?><br>Vash parol: <?=$pwd; ?>
+
 <!-- Deutsche -->
-Sehr geehrter Kunde! <br>Bei http://www.ruslania.com wurde Ihr  Wunsch angenommen, Ihr Kennwort zu liefern. Wir schicken das Kennwort an die von Ihnen bei Anmeldung angegebene E-Mail-Adresse. <br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+<? elseif (Yii::app()->language == 'de') : ?>
+Sehr geehrter Kunde! <br>Bei http://ruslania2.ptysh.ru wurde Ihr  Wunsch angenommen, Ihr Kennwort zu liefern. Wir schicken das Kennwort an die von Ihnen bei Anmeldung angegebene E-Mail-Adresse. <br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+
 <!-- Suomi -->
-Arvoisa asiakas! <br>Nettikauppaamme http://www.ruslania.com on saapunut pyynto toimittaa salasana. Lahetamme salasanan sahkopostiosoitteeseen, jonka olet antanut rekisteroityessasi.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+<? elseif (Yii::app()->language == 'fi') : ?>
+Arvoisa asiakas! <br>Nettikauppaamme http://ruslania2.ptysh.ru on saapunut pyynto toimittaa salasana. Lahetamme salasanan sahkopostiosoitteeseen, jonka olet antanut rekisteroityessasi.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+
 <!-- English -->
-Dear customer!<br>We have received to http://www.ruslania.com a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+<? elseif (Yii::app()->language == 'en') : ?>
+Dear customer!<br>We have received to http://ruslania2.ptysh.ru a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+
 <!-- France -->
-Dear customer!<br>We have received to http://www.ruslania.com a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
-<br><br><hr width=75%><br>
+<? elseif (Yii::app()->language == 'fr') : ?>
+Dear customer!<br>We have received to http://ruslania2.ptysh.ru a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+
 <!-- Espanol -->
-Dear customer!<br>We have received to http://www.ruslania.com a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+<? elseif (Yii::app()->language == 'es') : ?>
+Dear customer!<br>We have received to http://ruslania2.ptysh.ru a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+<? else : ?>
+Dear customer!<br>We have received to http://ruslania2.ptysh.ru a request to deliver your password. The password has been sent to the e-mail address that you used upon registration.<br><br>Login: <?=$login; ?><br>Password: <?=$pwd; ?>
+
+<? endif; ?>
