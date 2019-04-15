eadda1381a7234bdb7d32d6d637caf44ed1a93ed
kirill <kirill.ruh@gmail.com>
2019-04-15 20:51:53 +0400

    градиент

--- protected/components/widgets/views/banners_main_small.php
+++ protected/components/widgets/views/banners_main_small.php
@@ -8,12 +8,12 @@
 	?>
 		<div onclick="window.location.href = '<?= $url ?>'" class="span6 main-banner-content" style="background: url(/new_img/day_fon.jpg) 100% 100% no-repeat; background-size: contain; cursor: pointer;">
 			<div class="photo">
-				<div><a href="<?= $url ?>"><img src="<?= $productPicture ?>" alt=""/></a></div>
+				<div><img src="<?= $productPicture ?>" alt=""/></div>
 			</div>
 	<?php if (!empty($offerDay['priceData'][DiscountManager::DISCOUNT])) : ?>
 			<div class="discount"<?php if ($offerDay['entity'] == Entity::PERIODIC): ?> style="margin-top: 9px;" <?php endif; ?>><?= Yii::app()->ui->item('PRODUCT_OF_DAY_INFO', $offerDay['priceData'][DiscountManager::DISCOUNT]) ?></div>
 	<?php endif; ?>
-			<div class="title"><div><a href="<?= $url ?>"><?= $productTitle ?><?php /*<span class="gradient_link"></span>*/ ?></a></div></div>
+			<div class="title"><div id="js_offerDay"><?= mb_substr($productTitle, 0, 120, 'utf-8') ?></div></div>
 			<div class="cost_nds"<?php if ($offerDay['entity'] == Entity::PERIODIC): ?> style="line-height: 20px; width: 140px;" <?php endif; ?>>
 <?= ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::WITH_VAT]); ?> <?= $offerDay['priceData']['unit'] ?>
 				<span>(<span><?= trim(ProductHelper::FormatPrice($offerDay['priceData'][DiscountManager::BRUTTO]) . ' ' . $offerDay['priceData']['unit']) ?></span>)</span>
@@ -30,3 +30,24 @@
 <?php endif; ?>
 	</div>
 </div>
+<script type="text/javascript">
+	$(function() {
+		var i = 10;
+		function otrezat(str) {
+			console.log(str);
+			var lastIndex = str.lastIndexOf(" ");
+			return str.substring(0, lastIndex);
+		}
+
+		function findPhrase() {
+			i--;
+			var $block = $('#js_offerDay');
+			if (($block.outerHeight(1) > ($block.closest('div.title').outerHeight(1) + 1))&&(i > 0)) {
+				$block.html(otrezat($block.html()));
+				findPhrase();
+			}
+			else $block.html(otrezat($block.html()));
+		}
+		findPhrase();
+	});
+</script>
