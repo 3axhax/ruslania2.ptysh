65f25179f9861087b8cbd0becca5e6c4c352bde9
KirillRu <kirill.ruh@yandex.ru>
2019-08-01 23:11:02 +0400

    да-нет

--- protected/views/buy/forgot_button.php
+++ protected/views/buy/forgot_button.php
@@ -2,8 +2,8 @@
 <div>
 	<div><?= Yii::app()->ui->item('REGISTER_ERROR_LOGIN_IS_EXISTS') ?></div>
 	<div style="font-size: 17px; font-weight: bold;"><?= Yii::app()->ui->item('CARTNEW_ALERT_PSW') ?></div>
-	<input style="width: 50px; background-color: #0088cc; color: #fff" type="button" value="Да" onclick="forgotPassword('<?= $email ?>')" />
-	<input style="width: 50px; background-color: #0088cc; color: #fff" type="button" value="Нет" onclick="$('#Address_contact_email').val(''); $(this).closest('.info_box').html('');" />
+	<input style="width: 50px; background-color: #0088cc; color: #fff" type="button" value="<?= Yii::app()->ui->item('A_NEW_BTN_YES') ?>" onclick="forgotPassword('<?= $email ?>')" />
+	<input style="width: 50px; background-color: #0088cc; color: #fff" type="button" value="<?= Yii::app()->ui->item('A_NEW_BTN_NO') ?>" onclick="$('#Address_contact_email').val(''); $(this).closest('.info_box').html('');" />
 </div>
 <script>
 	function forgotPassword(email) {
